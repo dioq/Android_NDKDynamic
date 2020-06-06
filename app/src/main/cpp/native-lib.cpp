7b5b556d3aedb9b01781eef42ca68aab2beabb78
Dio <zhendong2011@live.cn>
2020-06-06 18:47:27 +0800

    delete
    删除无用文件,升级Gradle

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -28,7 +28,6 @@ static JNINativeMethod g_methods[] = {
 
 // must define this function
 JNIEXPORT int JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
-
     JNIEnv *env;
     if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
         return JNI_ERR;
--- app/src/main/jni/native-lib.c
+++
@@ -1,4 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jstring
-JNIEXPORT jstring
